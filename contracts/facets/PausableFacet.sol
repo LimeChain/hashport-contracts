6bfde65bcc0b134002b6d769b75db6d17227b522
failfmi <oscurocalma@gmail.com>
2021-08-31 10:10:10 +0300

    inline-docs(pausable): dev -> notice

--- contracts/facets/PausableFacet.sol
+++ contracts/facets/PausableFacet.sol
@@ -5,18 +5,18 @@ import "../libraries/LibDiamond.sol";
 import "../interfaces/IPausable.sol";
 
 contract PausableFacet is IPausable {
-    /// @dev Returns true if the contract is paused, and false otherwise
+    /// @notice Returns true if the contract is paused, and false otherwise
     function paused() external view override returns (bool) {
         return LibDiamond.paused();
     }
 
-    /// @dev Pauses the contract. Reverts if caller is not owner or already paused
+    /// @notice Pauses the contract. Reverts if caller is not owner or already paused
     function pause() external override {
         LibDiamond.enforceIsContractOwner();
         LibDiamond.pause();
     }
 
-    /// @dev Unpauses the contract. Reverts if the caller is not owner or already not paused
+    /// @notice Unpauses the contract. Reverts if the caller is not owner or already not paused
     function unpause() external override {
         LibDiamond.enforceIsContractOwner();
         LibDiamond.unpause();
--- contracts/interfaces/IPausable.sol
+++ contracts/interfaces/IPausable.sol
@@ -7,12 +7,12 @@ interface IPausable {
     // Emitted once the pause is lifted by an `account`
     event Unpaused(address indexed account);
 
-    /// @dev Returns true if the contract is paused, and false otherwise
+    /// @notice Returns true if the contract is paused, and false otherwise
     function paused() external view returns (bool);
 
-    /// @dev Pauses the contract. Reverts if caller is not owner or already paused
+    /// @notice Pauses the contract. Reverts if caller is not owner or already paused
     function pause() external;
 
-    /// @dev Unpauses the contract. Reverts if the caller is not owner or already not paused
+    /// @notice Unpauses the contract. Reverts if the caller is not owner or already not paused
     function unpause() external;
 }
